f0774aefa618fd2a8b0853dbe04d0979b091573a
João Carlos Valadares Ribeiro Filho <joaocvr@dataprev.gov.br>
2021-11-08 14:16:41 -0300

    Adding news routes with react-router!

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import AppBar from "./modules/views/appbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SingIn from "./modules/views/signin";
+import SingUp from "./modules/views/signup";
 import withRoot from "./modules/withRoot";
+import AppBar from "./modules/views/appbar";
 import News from "./modules/views/news";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <AppBar />
-      <News />
-    </div>
+      <Routes>
+        <Route path="/" element={<News />} />
+        <Route path="/sign-in" element={<SingIn />} />
+        <Route path="/sign-up" element={<SingUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
